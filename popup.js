a8ab0d26a3f91fec7f15a65005467a6f5afebbce
Sam Wray <hello@brkbrkbrk.com>
2015-04-29 15:16:42 +0100

    Adds setTimeout

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 var $ = document;
 
-$.addEventListener('DOMContentLoaded', function() {
+$.addEventListener('DOMContentLoaded', setTimeout(function() {
 	var iframe = $.createElement('iframe');
 	iframe.src = "http://dank.infinite.pizza/";
 	iframe.frameborder = "0";
 	iframe.allowfullscreen = "true";
 	$.body.appendChild(iframe);
-});
+}, 300));
